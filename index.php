020d116ce0a9644ceb4deaea57f3810b25c73507
windermanjd <manosit.winj@gmail.com>
2022-03-29 01:02:35 +0700

    edit

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 $dataset = json_decode(file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"),true);
 if(isset($_GET["act"])){
 if($_GET["act"]=="save"){
